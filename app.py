7ef41544522cd03354b9d5fc6107480107ab81b4
rodriguezmpg <rodriguez.mpg@gmail.com>
2024-12-27 16:07:02 +1100

    Update app.py

--- app.py
+++ app.py
@@ -3,23 +3,17 @@ import os
 import trading_loop
 import asyncio
 
-
 app=Flask(__name__)
-
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
 
 @app.route('/datos')
 def datos():
     return jsonify({
         'precio_banda': trading_loop.loop_class.precio_banda,
         'Cprecio': trading_loop.loop_class.current_price
-
     })
 
 @app.route('/main')
@@ -31,38 +25,10 @@ def start_trading():
     precio_banda_post = request.form.get('precio_banda_post_form')
     niveles_post_form = request.form.get('niveles_post_form')
     percentsl_post_form = request.form.get('percentsl_post_form')
-
     asyncio.run(trading_loop.start_socket(precio_banda_post, niveles_post_form, percentsl_post_form))
-
     return render_template('main_loop.html')
-
-
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True, use_reloader=False)
 
-
-   # try:
-        #     orden = client.futures_create_order(
-        #         symbol='ETHUSDT',           
-        #         side=SIDE_SELL,              
-        #         type=ORDER_TYPE_MARKET,     
-        #         quantity=0.01              
-        #     )
-        #     print(f"Orden abierta exitosamente: {orden}")
-
-        # except Exception as e:
-        #     print(f"Ocurrió un error: {e}")
-
-        # try:
-        #     orden = client.futures_create_order(
-        #         symbol='ETHUSDT',           
-        #         side=SIDE_BUY,              
-        #         type=ORDER_TYPE_MARKET,     
-        #         quantity=0.01              
-        #     )
-        #     print(f"Orden cerrada exitosamente: {orden}")
-
-        # except Exception as e:
-        #     print(f"Ocurrió un error: {e}")
