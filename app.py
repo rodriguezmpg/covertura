ce528efb40cace377a31b1153ffac6a4956a3b2e
rodriguezmpg <rodriguez.mpg@gmail.com>
2024-12-27 12:04:25 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from binance.client import Client
+from binance.enums import *
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+print "Hello"
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
