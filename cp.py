6c1efc17c20b4f58ca21ccd18282fddd0b41785c
rodriguezmpg <rodriguez.mpg@gmail.com>
2025-01-28 06:40:47 +0000

    sdf

--- cp.py
+++ cp.py
@@ -1,5 +1,6 @@
 from binance.client import Client
 from binance.enums import *
+import requests
 
 API_KEY = '5ucUUmRpHMuB4vZsNrSrVKKQDpWMQJ4el1dZFYZzJUpBZ70RQtT0OToZxenWHjhs'
 API_SECRET = '1Up2nryNOL2LZle6AjaSBiMwiC7L1wudS0u6RB001o1Vu1LlIEXPwsyIr1yI29GS'
@@ -27,12 +28,17 @@ def open_order():
 #Obtencion de la IP
 def get_public_ip():
     try:
-        response = requests.get('https://api.ipify.org')  # O usa https://api.ipify.org
-        data = response.json()
-        return data['origin']  # La IP estará bajo la clave 'origin'
+        response = requests.get('https://api.ipify.org?format=json')  # Usa este servicio
+        if response.status_code == 200:
+            data = response.json()
+            return data['ip']  # Asegúrate de que sea 'ip' y no 'origin'
+        else:
+            print(f"Error en la respuesta: {response.status_code}")
+            return None
     except Exception as e:
         print(f"Error al obtener la IP: {e}")
         return None
 
+print(get_public_ip())
 
 
